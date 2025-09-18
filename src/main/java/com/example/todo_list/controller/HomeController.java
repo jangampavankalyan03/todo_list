3e3149f3554cf52b30fa3583c1f9257c52ffadf6
Jangam Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-09-19 00:26:01 +0530

    Change the DB configuration to MySQL and add CORS

--- src/main/java/com/example/todo_list/controller/HomeController.java
+++ src/main/java/com/example/todo_list/controller/HomeController.java
@@ -3,10 +3,7 @@ package com.example.todo_list.controller;
 import com.example.todo_list.model.Lists;
 import com.example.todo_list.service.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +13,7 @@ public class HomeController {
     @Autowired
     HomeService service;
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/allNotes")
     public List<Lists> getAll() {
         return service.getAll();
